0b3f51604a9c4df9843e0a9453e61bea7180983e
Lauris <lauriselvijsm@gmail.com>
2022-07-20 12:42:12 +0300

    added line height

--- src/styles/shared/Text.style.ts
+++ src/styles/shared/Text.style.ts
@@ -10,6 +10,8 @@ export const SecondaryTextStyle = styled.h2`
   font-size: ${({ theme }) => theme.typography.fontSize.fontSizeSmall};
 `;
 export const ParagraphStyle = styled.p`
+  line-height: 26px;
+
   font-size: ${({ theme }) => theme.typography.fontSize.fontSizeMedium};
   font-weight: ${({ theme }) => theme.font.fontWeight.fontWeightMedium};
 
